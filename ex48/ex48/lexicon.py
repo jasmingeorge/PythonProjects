99e3ceb399e0c2c92e5d77a404e4e1e545bcffc2
jasmin <jasmingeorge21@gmail.com>
2014-11-13 22:14:57 +0530

    added ex48

--- ex48/ex48/lexicon.py
+++ ex48/ex48/lexicon.py
@@ -5,11 +5,11 @@ def scan(val):
 	for value in words:
 		if(value in {"north","south","east","west","down","up","left","right","back"}):
 			tuples.append(('direction',value))
-		elif(value in {"go","stop","kill","eat"}):
+		elif(value in {"run","go","stop","kill","eat"}):
 			tuples.append(('verb',value))
 		elif(value in {"the","in","of","from","at","it"}):
 			tuples.append(('stop',value))
-		elif(value in {"door","bear","princess","cabinet"}):
+		elif(value in {"honey","door","bear","princess","cabinet"}):
 			tuples.append(('noun',value))
 		elif(value in {'0','1','2','3','4','5','6','7','8','9'}):
 			tuples.append(('number', int(value)))
